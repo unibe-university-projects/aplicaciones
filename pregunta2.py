88da2e7c27a018523ec309d46b82d848d870dc0f
hscordonez <zh311505@gmail.com>
2024-01-27 11:44:53 -0500

    feat

---
+++ pregunta2.py
@@ -0,0 +1,9 @@
+# Se pide al estudiante que cree el siguiente programa con funciones o constructores en python 
+
+# Escribir un programa en el que se pregunte al usuario por una frase y una letra, y muestre por pantalla el número de veces que aparece la letra en la frase.
+
+#  Nota:
+
+# Si el programa cumple todos los requerimientos obtendra la calificacion de 1, si cumple la mitad del comportamiento 0.5, caso contrario se calificará con cero a esta pregunta.
+
+
