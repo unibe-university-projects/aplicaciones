7dc0a46387a354a306c35bdf5641badc6279f653
HECTOR STEVEEN ORDOÑEZ CHAMBA <hsc.ordonez@yavirac.edu.ec>
2024-02-11 18:42:45 -0500

    feat: deber

--- webapp/webapp/gestion_personas/admin.py
+++ webapp/webapp/gestion_personas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Persona
 
-# Register your models here.
+@admin.register(Persona)
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'edad', 'direccion', 'telefono')
+    search_fields = ('nombre', 'direccion', 'telefono')
--- webapp/webapp/gestion_usuarios/admin.py
+++ webapp/webapp/gestion_usuarios/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Usuario
 
-# Register your models here.
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre_usuario', 'email')
+    search_fields = ('nombre_usuario', 'email')
